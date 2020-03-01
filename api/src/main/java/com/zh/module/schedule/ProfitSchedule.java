eb7819c4102542ef33eab087c5dc6d78b80e2e8f
赵赫 <zgsdzczh@163.com>
2020-03-01 13:43:38 +0800

    commit

--- api/src/main/java/com/zh/module/schedule/ProfitSchedule.java
+++ api/src/main/java/com/zh/module/schedule/ProfitSchedule.java
@@ -25,6 +25,7 @@ public class ProfitSchedule {
 	 * 自动取消预约
 	 */
 //	@Scheduled(cron="1/5 * * * * ?")
+	@PostConstruct
 	public void cancelAppointment(){
 		petsListBiz.cancelAppointmentSchedule();
 	}
@@ -32,6 +33,7 @@ public class ProfitSchedule {
 	 * 自动取消未付款
 	 */
 //	@Scheduled(cron="1/5 * * * * ?")
+	@PostConstruct
 	public void cancelNoPay(){
 		petsListBiz.cancelNoPaySchedule();
 	}
@@ -39,6 +41,7 @@ public class ProfitSchedule {
 	 * 自动确认未确认
 	 */
 //	@Scheduled(cron="1/5 * * * * ?")
+	@PostConstruct
 	public void cancelNoConfirm(){
 		petsListBiz.cancelNoConfirmSchedule();
 	}
---
+++ api/src/test/java/com/zh/module/PetsListBizTests.java
@@ -0,0 +1,77 @@
+package com.zh.module;
+
+import com.zh.module.biz.PetsBiz;
+import com.zh.module.biz.PetsListBiz;
+import com.zh.module.biz.PetsMatchingListBiz;
+import com.zh.module.biz.UsersBiz;
+import com.zh.module.constants.GlobalParams;
+import com.zh.module.constants.SmsTemplateCode;
+import com.zh.module.entity.Pets;
+import com.zh.module.entity.PetsList;
+import com.zh.module.entity.Users;
+import com.zh.module.model.PageModel;
+import com.zh.module.service.PetsListService;
+import com.zh.module.service.PetsService;
+import com.zh.module.service.UsersService;
+import com.zh.module.utils.DateUtils;
+import com.zh.module.utils.FeigeSmsUtils;
+import com.zh.module.utils.RedisUtil;
+import com.zh.module.variables.RedisKey;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.text.ParseException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PetsListBizTests {
+
+    @Autowired
+    private PetsBiz petsBiz;
+    @Autowired
+    private PetsMatchingListBiz petsMatchingListBiz;
+    @Autowired
+    private PetsListBiz petsListBiz;
+    @Autowired
+    private UsersService usersService;
+    @Autowired
+    private UsersBiz usersBiz;
+    @Autowired
+    private PetsService petsService;
+    @Autowired
+    private PetsListService petsListService;
+    @Autowired
+    private RedisTemplate<String, String> redis;
+
+    /**
+     * 自动取消预约
+     */
+    @Test
+    public void cancelNoPaySchedule(){
+        petsListBiz.cancelAppointmentSchedule();
+    }
+
+    /**
+     * 自动取消未付款
+     */
+    @Test
+    public void cancelNoConfirmSchedule(){
+        petsListBiz.cancelNoPaySchedule();
+    }
+
+    /**
+     * 自动确认未确认
+     */
+    @Test
+    public void censusAppointment(){
+        petsListBiz.cancelNoConfirmSchedule();
+    }
+}
