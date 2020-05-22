a1f43633e9c5c10e133b1286673a0f2f3f7aed29
赵赫 <zgsdzczh@163.com>
2020-05-22 16:29:16 +0800

    commit

--- api/src/test/java/com/zh/module/UserBizTests.java
+++ api/src/test/java/com/zh/module/UserBizTests.java
@@ -2,12 +2,22 @@ package com.zh.module;
 
 import com.zh.module.aliyun.H5RpBasic;
 import com.zh.module.biz.UsersBiz;
+import com.zh.module.constants.AccountType;
+import com.zh.module.constants.CoinType;
+import com.zh.module.entity.Account;
 import com.zh.module.entity.Users;
+import com.zh.module.service.AccountService;
+import com.zh.module.service.UsersService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -15,6 +25,10 @@ public class UserBizTests {
 
     @Autowired
     private UsersBiz usersBiz;
+    @Autowired
+    private AccountService accountService;
+    @Autowired
+    private UsersService usersService;
     @Autowired
     private H5RpBasic h5RpBasic;
     @Test
@@ -53,6 +67,22 @@ public class UserBizTests {
     @Test
     public void realName2() throws Exception {
         Users users = usersBiz.getUser(1);
+        System.out.println(usersBiz.getStatus(1));
+    }
+    @Test
+    public void blackList() {
+        Map<Object, Object> param = new HashMap<>();
+        param.put("coinType", CoinType.OS);
+        param.put("accountType", AccountType.ACCOUNT_TYPE_ACTIVE);
+        List<Account> list = accountService.selectAll(param);
+        for(Account account : list){
+            if(account.getAvailbalance().compareTo(new BigDecimal("200")) < 0) {
+                Users users = usersBiz.getUser(account.getUserId());
+                users.setState((byte) 3);
+                usersService.updateByPrimaryKeySelective(users);
+            }
+
+        }
         System.out.println(usersBiz.getStatus(1));
     }
 
